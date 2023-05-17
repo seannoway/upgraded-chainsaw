947d5ddde23edb4427ce85ee2a72f5b8d7ca0be9
Sean Noway <sean.m.holian@gmail.com>
2023-05-16 20:09:45 -0400

    finished User model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const Student = require('./Thought');
-const Course = require('./User');
+const Thought = require('./Thought');
+const User = require('./User');
 
 module.exports = { User, Thought };
